7049f61f651b16dbc16d4898e74bacf2cec65142
patriciosanchezs <patriciosanchez@MacBook-Pro-de-Patricio.local>
2021-05-11 18:13:40 -0400

    primer commit

---
+++ pixabay/src/app/app.module.ts
@@ -0,0 +1,33 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { AppRoutingModule } from './app-routing.module';
+import { FormsModule } from '@angular/forms';
+import {HttpClientModule} from '@angular/common/http'
+
+//componentes
+import { AppComponent } from './app.component';
+import { BuscarImagenComponent } from './components/buscar-imagen/buscar-imagen.component';
+import { ListarImagenComponent } from './components/listar-imagen/listar-imagen.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { SpinnerComponent } from './shared/spinner/spinner.component';
+import { ErrorComponent } from './shared/error/error.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    BuscarImagenComponent,
+    ListarImagenComponent,
+    NavbarComponent,
+    SpinnerComponent,
+    ErrorComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    FormsModule,
+    HttpClientModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ pixabay/src/app/components/buscar-imagen/buscar-imagen.component.ts
@@ -0,0 +1,24 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-buscar-imagen',
+  templateUrl: './buscar-imagen.component.html',
+  styleUrls: ['./buscar-imagen.component.css']
+})
+export class BuscarImagenComponent implements OnInit {
+
+  nombreImagen: string;
+
+
+  constructor() {
+    this.nombreImagen = '';
+   }
+
+  ngOnInit(): void {
+  }
+
+
+  buscarImagenes() {
+    console.log(this.nombreImagen);
+  }
+}
